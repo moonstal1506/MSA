549c338e4a750424158564d2e367ee91fe0913c1
moonstal1506 <moonstal1506@gmail.com>
2023-08-23 08:02:56 +0900

    OrderController health check

---
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/order-service")
+public class OrderController {
+
+    private final Environment env;
+    private final OrderService orderService;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's Working in Order Service on PORT %s",
+                env.getProperty("local.server.port"));
+    }
+}
