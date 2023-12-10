753ce25140dc7f3602f9f3a0a180960ca110d0f8
moonstal1506 <moonstal1506@gmail.com>
2023-12-10 17:26:19 +0900

    Users Microservice에서 Spring Cloud Config 연동

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -24,6 +24,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
                 .permitAll()
                 .and()
